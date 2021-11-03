eeb460ad438434e1017d1599bbacedee4f513c7e
Ahmed Hany <ahmedhanyhosny@outlook.com>
2021-11-03 12:22:34 +0200

    Fixed a bug - Added a new feature
    Bug: .hasChildNodes should be a method (i.e. should be .hasChildNodes()), Feature: generate a new grid by getting input from the user

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
 const container = document.querySelector("#container");
 
-let dim = 1024 / 16;
-for (let i = 0; i < 256; i++) {
-    let div = document.createElement("div");
-    div.style.width = `${dim}px`;
-    div.style.height = `${dim}px`;
-    container.appendChild(div);
+function generateGrid(numOfSquaresPerSide = 16) {
+    
+    let dim = 1024 / numOfSquaresPerSide;
+    let totalNumOfSquares = numOfSquaresPerSide ** 2;
+    
+    for (let i = 0; i < totalNumOfSquares; i++) {
+        let div = document.createElement("div");
+        div.style.width = `${dim}px`;
+        div.style.height = `${dim}px`;
+        container.appendChild(div);
+    }
 }
+
+generateGrid();
 
 container.addEventListener("mouseover", e => {
     e.target.style.backgroundColor = "red";
@@ -14,9 +21,14 @@ container.addEventListener("mouseover", e => {
 
 
 function clearGrid(e) {
-    while (container.hasChildNodes) {
+    
+    while (container.hasChildNodes()) {
         container.removeChild(container.firstElementChild);
     }
+    
+    const numOfSquares = +prompt("Enter the number of squares per side for the new grid:");
+    
+    generateGrid(numOfSquares);
 }
 
 const clearGridButton = document.querySelector("#clear-grid-btn");
